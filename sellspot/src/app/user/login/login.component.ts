378d3a1b35ec22a7e506f07f8d64cadeb2fc37f9
ivan9paunov <vankatapaunov@gmail.com>
2024-12-06 22:30:09 +0200

    Switch from Template-Driven Approach to Reactive Approach in Login component

--- sellspot/src/app/user/login/login.component.ts
+++ sellspot/src/app/user/login/login.component.ts
@@ -1,26 +1,44 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../user.service';
-import { FormsModule, NgForm } from '@angular/forms';
-import { EmailDirective } from '../../directives/email.directive';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { emailValidator } from '../../utils/email.validator';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, FormsModule, EmailDirective],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private userService: UserService, private router: Router) {}
+  // constructor(private userService: UserService, private router: Router) {}
 
-  login(form: NgForm) {
-    if (form.invalid) {
-      console.error('Invalid Login form!')
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, emailValidator()]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  inputMissing(controlName: string) {
+    return this.form.get(controlName)?.touched && this.form.get(controlName)?.errors?.['required']
+  }
+
+  get emailNotValid() {
+    return this.form.get('email')?.touched && this.form.get('email')?.errors?.['emailValidator']
+  }
+
+  get isUnderMinLength() {
+    return this.form.get('password')?.touched && this.form.get('password')?.errors?.['minlength'];
+  }
+
+  login() {
+    if (this.form.invalid) {
       return;
     }
 
-    this.userService.login();
-    this.router.navigate(['/home']);
+    console.log(this.form.value);
+    
+    // this.userService.login();
+    // this.router.navigate(['/home']);
   }
 }
