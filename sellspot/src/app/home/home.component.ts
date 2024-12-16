6921c016bc27f9a2df13f55ad67b2cec7429f098
ivan9paunov <vankatapaunov@gmail.com>
2024-12-16 18:18:13 +0200

    Home page - redirect to server-error page if error from server;  unsubscribe using ngOnDestroy

--- sellspot/src/app/home/home.component.ts
+++ sellspot/src/app/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Game } from '../types/game';
 import { LoaderComponent } from '../shared/loader/loader.component';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -11,16 +12,30 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   games: Game[] = [];
   isLoading = true;
+  private subscriptions: Subscription[] = [];
 
-  constructor(private apiService: ApiService) {}
-
+  constructor(private apiService: ApiService, private router: Router) { }
+  
   ngOnInit(): void {
-    this.apiService.getLastThreeGames().subscribe((games) => {
-      this.games = games;
-      this.isLoading = false;
-    });
+    const getLastThreeGamesSub = this.apiService.getLastThreeGames().subscribe(
+      (games) => {
+        this.games = games;
+        this.isLoading = false;
+      },
+      (error) => {
+        console.error('Error loading recent games', error);
+        this.isLoading = false;
+        this.router.navigate(['/server-error']);
+      }
+    );
+
+    this.subscriptions.push(getLastThreeGamesSub);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 }
